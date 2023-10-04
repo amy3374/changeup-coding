8e342e7685105be5005b8abd6363072eb832324b
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-10-04 22:58:06 +0900

    2주차

--- 2주차.py
+++ 2주차.py
@@ -53,3 +53,19 @@ def solution(maps):
             return maps[len(maps)-1][len(maps[0])-1]
     answer = bfs(0, 0)
     return answer
+
+# [DP - 정수 삼각형]
+
+
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == len(triangle[i])-1:
+                triangle[i][j] += triangle[i-1][-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
+
+    answer = max(triangle[-1])
+    return answer
