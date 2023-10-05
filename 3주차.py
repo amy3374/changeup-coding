522d710af69af04baa6174643598beb5b6808b14
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-10-05 11:18:23 +0900

    3주차

---
+++ 3주차.py
@@ -0,0 +1,11 @@
+# [해시 - 의상]
+def solution(clothes):
+    hash_map = {}
+    for cloth, type in clothes:
+        hash_map[type] = hash_map.get(type, 0) + 1
+
+    answer = 1
+    for type in hash_map:
+        answer *= (hash_map[type] + 1)
+
+    return answer - 1
