679bf7b06b5b405bf812ad89b9fd49bf062f70fa
amy3374 <119571479+amy3374@users.noreply.github.com>
2023-10-16 23:37:49 +0900

    4주차

--- 4주차.py
+++ 4주차.py
@@ -1,4 +1,4 @@
-
+# [깊이/너비 우선탐색(DFS/BFS) - 단어변환]
 def bfs(begin, target, words):
 
     queue = deque()
@@ -18,3 +18,22 @@ def bfs(begin, target, words):
 
             if temp_cnt == 1:
                 queue.append([word, cnt+1])
+
+# [그리디 - 단어변환]
+
+
+def solution(people, limit):
+    people.sort()
+    answer = 0
+    start = 0
+    end = len(people) - 1  # 시작과 끝 지정
+    while start <= end:
+        # 최소 + 최대로 2명 태울 수 있는 경우
+        if people[start] + people[end] <= limit:
+            start += 1
+            end -= 1
+            answer += 1
+        else:
+            end -= 1
+            answer += 1
+    return answer
