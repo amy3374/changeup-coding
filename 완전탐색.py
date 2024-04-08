d1e6edc8a1f0291bef2efe413fb8b7d539104790
amy3374 <119571479+amy3374@users.noreply.github.com>
2024-04-08 22:19:32 +0900

    완전탐색- 카펫

--- 완전탐색.py
+++ 완전탐색.py
@@ -1,23 +1,14 @@
 # [완전탐색 - 카펫]
-# 정확성은 ok, but 효율성 문제
 def solution(brown, yellow):
     answer = []
-    sum = brown + yellow
-    for i in range(1, sum+1):
-        if sum % i == 0:
-            answer.append((i, int(sum//i)))
-    return answer[len(answer)//2]
-
-# 검색한 풀이
-
-
-def solution(brown, yellow):
-    answer = []
-    sum = brown + yellow
-    for b in range(1, sum+1):
-        if sum % b == 0:
-            a = sum//b
-            if a >= b:
-                if 2*a + 2*b - 4 == brown:
-                    return [a, b]
+    width = 0
+    height = 0
+    total = brown+yellow
+    for i in range(1,total):
+        if total % i == 0:
+            width = total//i
+            height = i
+            if width >= height and (width-2)*(height-2)==yellow:
+                return [width,height]
+    
     return answer
